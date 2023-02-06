93f16f9ca62a03405edc69b7230e34867b644939
LukaPavicic1 <91119924+LukaPavicic1@users.noreply.github.com>
2023-02-06 12:43:12 +0100

    Add files via upload

---
+++ Aplikacija-za-biblioteku-main/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace aplikacijaZaBiblioteku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonKorisnik = new System.Windows.Forms.Button();
+            this.buttonKnjiga = new System.Windows.Forms.Button();
+            this.buttonPregledPodataka = new System.Windows.Forms.Button();
+            this.buttonPosudzivanjeVracanje = new System.Windows.Forms.Button();
+            this.labelNaslov = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonKorisnik
+            // 
+            this.buttonKorisnik.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.buttonKorisnik.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonKorisnik.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonKorisnik.Location = new System.Drawing.Point(34, 178);
+            this.buttonKorisnik.Name = "buttonKorisnik";
+            this.buttonKorisnik.Size = new System.Drawing.Size(266, 62);
+            this.buttonKorisnik.TabIndex = 0;
+            this.buttonKorisnik.Text = "Unesi korisnika";
+            this.buttonKorisnik.UseVisualStyleBackColor = false;
+            this.buttonKorisnik.Click += new System.EventHandler(this.buttonKorisnik_Click);
+            // 
+            // buttonKnjiga
+            // 
+            this.buttonKnjiga.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.buttonKnjiga.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonKnjiga.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonKnjiga.Location = new System.Drawing.Point(34, 285);
+            this.buttonKnjiga.Name = "buttonKnjiga";
+            this.buttonKnjiga.Size = new System.Drawing.Size(266, 62);
+            this.buttonKnjiga.TabIndex = 1;
+            this.buttonKnjiga.Text = "Unesi knjigu";
+            this.buttonKnjiga.UseVisualStyleBackColor = false;
+            this.buttonKnjiga.Click += new System.EventHandler(this.buttonKnjiga_Click);
+            // 
+            // buttonPregledPodataka
+            // 
+            this.buttonPregledPodataka.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.buttonPregledPodataka.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPregledPodataka.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonPregledPodataka.Location = new System.Drawing.Point(399, 178);
+            this.buttonPregledPodataka.Name = "buttonPregledPodataka";
+            this.buttonPregledPodataka.Size = new System.Drawing.Size(266, 62);
+            this.buttonPregledPodataka.TabIndex = 2;
+            this.buttonPregledPodataka.Text = "Pregled podataka";
+            this.buttonPregledPodataka.UseVisualStyleBackColor = false;
+            // 
+            // buttonPosudzivanjeVracanje
+            // 
+            this.buttonPosudzivanjeVracanje.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.buttonPosudzivanjeVracanje.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPosudzivanjeVracanje.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonPosudzivanjeVracanje.Location = new System.Drawing.Point(399, 285);
+            this.buttonPosudzivanjeVracanje.Name = "buttonPosudzivanjeVracanje";
+            this.buttonPosudzivanjeVracanje.Size = new System.Drawing.Size(266, 62);
+            this.buttonPosudzivanjeVracanje.TabIndex = 3;
+            this.buttonPosudzivanjeVracanje.Text = "Posuđivanje/Vraćanje knjiga";
+            this.buttonPosudzivanjeVracanje.UseVisualStyleBackColor = false;
+            this.buttonPosudzivanjeVracanje.Click += new System.EventHandler(this.buttonPosudzivanjeVracanje_Click);
+            // 
+            // labelNaslov
+            // 
+            this.labelNaslov.AutoSize = true;
+            this.labelNaslov.BackColor = System.Drawing.Color.Transparent;
+            this.labelNaslov.Font = new System.Drawing.Font("Sitka Heading", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelNaslov.ForeColor = System.Drawing.Color.DarkGreen;
+            this.labelNaslov.Location = new System.Drawing.Point(19, 40);
+            this.labelNaslov.Name = "labelNaslov";
+            this.labelNaslov.Size = new System.Drawing.Size(586, 87);
+            this.labelNaslov.TabIndex = 4;
+            this.labelNaslov.Text = "Knjižnica \"Laganini\"";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(714, 395);
+            this.Controls.Add(this.labelNaslov);
+            this.Controls.Add(this.buttonPosudzivanjeVracanje);
+            this.Controls.Add(this.buttonPregledPodataka);
+            this.Controls.Add(this.buttonKnjiga);
+            this.Controls.Add(this.buttonKorisnik);
+            this.Name = "Form1";
+            this.Text = "Početna";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonKorisnik;
+        private System.Windows.Forms.Button buttonKnjiga;
+        private System.Windows.Forms.Button buttonPregledPodataka;
+        private System.Windows.Forms.Button buttonPosudzivanjeVracanje;
+        private System.Windows.Forms.Label labelNaslov;
+    }
+}
+
---
+++ Aplikacija-za-biblioteku-main/UnosKnjiga.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace aplikacijaZaBiblioteku
+{
+    partial class UnosKnjiga
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelNaslov = new System.Windows.Forms.Label();
+            this.labelAut = new System.Windows.Forms.Label();
+            this.labelGod = new System.Windows.Forms.Label();
+            this.labelCijena = new System.Windows.Forms.Label();
+            this.labelDatum = new System.Windows.Forms.Label();
+            this.labelStanje = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.buttonUnesi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelNaslov
+            // 
+            this.labelNaslov.AutoSize = true;
+            this.labelNaslov.Location = new System.Drawing.Point(13, 13);
+            this.labelNaslov.Name = "labelNaslov";
+            this.labelNaslov.Size = new System.Drawing.Size(43, 13);
+            this.labelNaslov.TabIndex = 0;
+            this.labelNaslov.Text = "Naslov:";
+            // 
+            // labelAut
+            // 
+            this.labelAut.AutoSize = true;
+            this.labelAut.Location = new System.Drawing.Point(10, 61);
+            this.labelAut.Name = "labelAut";
+            this.labelAut.Size = new System.Drawing.Size(61, 13);
+            this.labelAut.TabIndex = 1;
+            this.labelAut.Text = "Ime Autora:";
+            // 
+            // labelGod
+            // 
+            this.labelGod.AutoSize = true;
+            this.labelGod.Location = new System.Drawing.Point(10, 117);
+            this.labelGod.Name = "labelGod";
+            this.labelGod.Size = new System.Drawing.Size(83, 13);
+            this.labelGod.TabIndex = 2;
+            this.labelGod.Text = "Godina izdanja::";
+            // 
+            // labelCijena
+            // 
+            this.labelCijena.AutoSize = true;
+            this.labelCijena.Location = new System.Drawing.Point(13, 165);
+            this.labelCijena.Name = "labelCijena";
+            this.labelCijena.Size = new System.Drawing.Size(39, 13);
+            this.labelCijena.TabIndex = 3;
+            this.labelCijena.Text = "Cijena:";
+            // 
+            // labelDatum
+            // 
+            this.labelDatum.AutoSize = true;
+            this.labelDatum.Location = new System.Drawing.Point(10, 220);
+            this.labelDatum.Name = "labelDatum";
+            this.labelDatum.Size = new System.Drawing.Size(85, 13);
+            this.labelDatum.TabIndex = 4;
+            this.labelDatum.Text = "Datum posudbe:";
+            // 
+            // labelStanje
+            // 
+            this.labelStanje.AutoSize = true;
+            this.labelStanje.Location = new System.Drawing.Point(13, 268);
+            this.labelStanje.Name = "labelStanje";
+            this.labelStanje.Size = new System.Drawing.Size(40, 13);
+            this.labelStanje.TabIndex = 5;
+            this.labelStanje.Text = "Stanje:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 317);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Broj posuđenih:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(246, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 133);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(246, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(13, 181);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(247, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(13, 236);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(244, 20);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(13, 284);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(246, 20);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(11, 333);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(246, 20);
+            this.textBox7.TabIndex = 13;
+            // 
+            // buttonUnesi
+            // 
+            this.buttonUnesi.Location = new System.Drawing.Point(73, 370);
+            this.buttonUnesi.Name = "buttonUnesi";
+            this.buttonUnesi.Size = new System.Drawing.Size(129, 44);
+            this.buttonUnesi.TabIndex = 14;
+            this.buttonUnesi.Text = "Unesi";
+            this.buttonUnesi.UseVisualStyleBackColor = true;
+            // 
+            // UnosKnjiga
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 426);
+            this.Controls.Add(this.buttonUnesi);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelStanje);
+            this.Controls.Add(this.labelDatum);
+            this.Controls.Add(this.labelCijena);
+            this.Controls.Add(this.labelGod);
+            this.Controls.Add(this.labelAut);
+            this.Controls.Add(this.labelNaslov);
+            this.Name = "UnosKnjiga";
+            this.Text = "UnosKnjiga";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNaslov;
+        private System.Windows.Forms.Label labelAut;
+        private System.Windows.Forms.Label labelGod;
+        private System.Windows.Forms.Label labelCijena;
+        private System.Windows.Forms.Label labelDatum;
+        private System.Windows.Forms.Label labelStanje;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button buttonUnesi;
+    }
+}
